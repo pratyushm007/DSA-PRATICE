d5deff5102c3e2691ed16233046a77d5a80602f9
Pratyush Mohapatra <pratyushmohapatra@batoibhai.com>
2025-10-26 15:06:52 +0530

    Add solution for LeetCode 344 Reverse String

---
+++ ReverseString_344.java
@@ -0,0 +1,27 @@
+//leetCode problem no344
+
+class Solution{
+    public void reverseString(char[] s) {
+      reverseString(s, 0);
+    }
+      private void reverseString(char[] s, int l) {
+         if (l >=s.length/2) return;
+         char temp = s[l];
+         s[l] = s[s.length-l-1];
+         s[s.length-l-1] = temp;
+         reverseString(s, l + 1);
+     }
+    
+ }
+ public class ReverseString_344{
+     public static void main(String[] args){
+      Solution obj=new Solution();
+      char arr[]=new char[]{'H','e','e','p','f'};
+      obj.reverseString(arr);
+      // on a char[] array may not always print as expected. It often prints the memory reference like [C@1b6d3586.
+      System.out.println( new String(arr));
+      
+     }
+
+ }
+ 
